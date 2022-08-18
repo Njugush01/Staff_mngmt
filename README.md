12a48915200d4ea35997677d0e7eef806c0ce346
Njugush01 <elvisnjuguna97@gmail.com>
2022-08-18 18:52:32 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,23 +1,4 @@
-# Rust Smart Contract Template
+STAFF MANAGEMENT SYSTEM
+OVERVIEW
 
-## Getting started
-
-To get started with this template:
-
-1. Click the "Use this template" button to create a new repo based on this template
-2. Update line 2 of `Cargo.toml` with your project name
-3. Update line 4 of `Cargo.toml` with your project author names
-4. Set up the [prerequisites](https://github.com/near/near-sdk-rs#pre-requisites)
-5. Begin writing your smart contract in `src/lib.rs`
-6. Test the contract 
-
-    `cargo test -- --nocapture`
-
-8. Build the contract
-
-    `RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release`
-
-**Get more info at:**
-
-* [Rust Smart Contract Quick Start](https://docs.near.org/develop/prerequisites)
-* [Rust SDK Book](https://www.near-sdk.io/)
+Staff Management System Rust Smart Contract creates a platform where employers can easily monitor their members of staff by either adding a new staff member to the system, shifting an existing staff to another department and even monitor the attendance of the members of staff.
--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,174 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
+use near_sdk::collections::Vector;
+use near_sdk::{near_bindgen, env};
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Contract {
-    // SETUP CONTRACT STATE
+
+//Capturing staff details
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+pub struct Staff {
+    staff_id: String,
+    name: String,
+    department: String,
+    roster:Vector<Roster>,
+    
+    
+}
+
+
+
+
+impl Default for Staff {
+    fn default() -> Self{
+        Staff { staff_id: "".to_string(), name: "".to_string(), department: "".to_string(), roster: Vector::new(b"r".to_vec()), }
+
+    }
+}
+
+
+pub struct department{
+    department_id: u32,
+    department_name: String,
+    role: String,
+}
+
+impl Default for department{
+    fn default() -> Self {
+        department{
+            department_id: 0,
+            department_name: "".to_string(),
+            role: "".to_string(),
+        }
+    }
+}
+
+pub struct roster{
+    date:String,
+    attended: String,
 }
 
 #[near_bindgen]
 impl Contract {
-    // ADD CONTRACT METHODS HERE
+    #[init]
+    pub fn new()-> Self{
+        let staff:Vector<Staff> = Vector::new(b"r".to_vec());
+        let departments:Vector<Department> = Vector::new(b"r".to_vec());
+
+
+        Contract{staff,departments}
+    }
+    
 }
 
-/*
- * the rest of this file sets up unit tests
- * to run these, the command will be:
- * cargo test --package rust-template -- --nocapture
- * Note: 'rust-template' comes from Cargo.toml's 'name' key
- */
+//function to add new staff
+pub fn add_new_staff(
+    self:&mut self,
+    staff_id: String,
+    name: String,
+    department: String,  
+){
+    let departments = &mut self.departments;
+    let mut counter = 0;
 
-// use the attribute below for unit tests
+    departments.iter() .for_each |department|{
+        if department.department_name == staff_department{
+            counter = counter+1;
+        }
+    };
+    env::log_str(format!("===> Counter {}",counter).as_str());
+
+ if counter > 0 {
+    let staff1 = Staff {
+        staff_id: staff_id.to_string(),
+        name: name.to_string(),
+        department: department.to_string(),
+        roster: Vector::new(b"r".to_vec()),
+        staff_id,
+    };
+
+    self.staff.push(&staff1);
+    env::log_str("successfully added!");
+ }else{
+    env::log_str("error department not available!");
+ }
+}
+
+pub fn get_staff_length(&self) -> u64{
+    self.staff.len();
+}
+
+pub fn get_staff(&self, staff_id: String) -> Option<Staff>{
+    let me:&Vector<Staff> = &self.staff;
+    let todo = me.iter().find(|tod|tod.staff_id == staff_id);
+    return todo;
+
+}
+//function to add a department
+
+pub fn add_department(&mut self, department_name:String){
+    let id = self.department.len() as u32;
+    let new_department = Department{
+        department_id: id,
+        department_name: department_name.to_string(),
+        role: Vector::new(b"r".to_vec()),
+    };
+
+    self.depatments.push(&new_department);
+    env::log_str("department successfully added!");
+}
+    
+pub fn get_department_length(&self)-> u64{
+    self.departments.len()
+}
+
+//funtion to record staff roster
+pub fn record_roster(&mut self, staff_id: String, attended:String) {
+    let mut staff_count = 0;
+    for (pos,mut staff) in self.staff.iter().enumerate(){
+        print!("The staff at position {}: {:?}",pos,staff);
+        if staff.staff_id == staff_id{
+            staff_count = staff_count+1;
+
+            let new_roster = Roster {
+                date: date.to_string(),
+                attended: attended.to_string(),
+            };
+
+            staff.roster.push(&new_roster);
+            env::log_str("Successfully recorded!");
+
+        }
+    }
+
+    if staff_count < 1{
+        env::log_str("Invalid staff_id! \n Please try again:");
+    }
+}
+
+
+    
+
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use near_sdk::test_utils::{get_logs, VMContextBuilder};
-    use near_sdk::{testing_env, AccountId};
+   // use near_sdk::{testing_env, AccountId};
 
-    // part of writing unit tests is setting up a mock context
-    // provide a `predecessor` here, it'll modify the default context
-    fn get_context(predecessor: AccountId) -> VMContextBuilder {
-        let mut builder = VMContextBuilder::new();
-        builder.predecessor_account_id(predecessor);
-        builder
+    #[test]
+    fn add_department(){
+        let mut department1 = Contract::new();
+        department1.add_department("Radiology".to_string());
+        department1.add_department("Admin".to_string());
+        department1.add_department("Nursing Services".to_string());
+        assert_eq!(department1.get_department_length(),3);
+
     }
+
 
     // TESTS HERE
 }
+ 
